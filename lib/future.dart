6a71d5d963a11eba841f51546e49570d67b813ce
Ivan Bacharnikov <53704523+Bachar-official@users.noreply.github.com>
2023-01-04 21:44:10 +0500

    async init commit

---
+++ lib/future.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FutureScreen extends StatelessWidget {
+  FutureScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Future'),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_education/dio.dart';
+import 'package:flutter_education/future.dart';
+import 'package:flutter_education/http.dart';
+import 'package:flutter_education/json.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,54 +17,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+  const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  int pageNumber = 0;
+
+  void setPage(int page) {
+    pageNumber = page;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: setPage,
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.adb_sharp), label: 'Future'),
+          NavigationDestination(icon: Icon(Icons.adb_sharp), label: 'HTTP'),
+          NavigationDestination(icon: Icon(Icons.adb_sharp), label: 'DIO'),
+          NavigationDestination(icon: Icon(Icons.adb_sharp), label: 'JSON'),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: [
+        FutureScreen(),
+        HttpScreen(),
+        DioScreen(),
+        JsonScreen()
+      ][pageNumber],
     );
   }
 }
