6a3e2c8448d868893544e53d33680ed6dec861c8
Ivan Bacharnikov <53704523+Bachar-official@users.noreply.github.com>
2023-01-05 12:34:32 +0500

    Keys example

---
+++ lib/keys.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class KeysExample extends StatefulWidget {
+  KeysExample();
+
+  @override
+  State<KeysExample> createState() => _KeysExampleState();
+}
+
+//key: UniqueKey()
+
+class _KeysExampleState extends State<KeysExample> {
+  List<Widget> containers = [StatefulColorfulTile(), StatefulColorfulTile()];
+
+  void swapContainers() {
+    containers.insert(0, containers.removeAt(0)); //Не замена, а модификация
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Keys'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: containers,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: swapContainers,
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+}
+
+Color getRandomColor() =>
+    Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+
+class StatefulColorfulTile extends StatefulWidget {
+  //StatefulColorfulTile({Key? key}): super(key: key);
+
+  @override
+  ColorfulTileState createState() => ColorfulTileState();
+}
+
+class ColorfulTileState extends State<StatefulColorfulTile> {
+  late Color myColor;
+
+  @override
+  void initState() {
+    super.initState();
+    myColor = getRandomColor();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: myColor,
+      child: const Padding(
+        padding: EdgeInsets.all(70.0),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_education/async_widget.dart';
+import 'package:flutter_education/builders.dart';
+import 'package:flutter_education/keys.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,6 +9,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,54 +17,45 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int pageNumber = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void setPage(int page) {
+    pageNumber = page;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: setPage,
+        selectedIndex: pageNumber,
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.access_time), label: 'Keys'),
+          NavigationDestination(
+              icon: Icon(Icons.access_time), label: 'Builders'),
+          NavigationDestination(
+              icon: Icon(Icons.access_time), label: 'Async Widget'),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: [
+        KeysExample(),
+        BuildersExample(),
+        AsyncWidgetExample(),
+      ][pageNumber],
     );
   }
 }
