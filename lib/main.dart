4a3fb0c63dcefa064d03dcce6802268bfaba01c4
Ivan Bacharnikov <53704523+Bachar-official@users.noreply.github.com>
2023-01-06 18:19:57 +0500

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_education/database.dart';
+import 'package:flutter_education/file.dart';
+import 'package:flutter_education/hive.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,6 +9,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,54 +17,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int page = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void setPage(int newPage) {
+    page = newPage;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: page,
+          onDestinationSelected: setPage,
+          destinations: const [
+            NavigationDestination(
+                icon: Icon(Icons.file_present), label: 'File'),
+            NavigationDestination(
+                icon: Icon(Icons.hive_outlined), label: 'Hive'),
+            NavigationDestination(icon: Icon(Icons.dataset), label: 'Database'),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: [
+          FileExample(),
+          HiveExample(),
+          DatabaseExample(),
+        ][page]);
   }
 }
