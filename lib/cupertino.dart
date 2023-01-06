37ee2a990817d82d29c0f3925b9ef818cd933ba0
Ivan Bacharnikov <53704523+Bachar-official@users.noreply.github.com>
2023-01-06 17:17:26 +0500

    extended cupertino

--- lib/cupertino.dart
+++ lib/cupertino.dart
@@ -11,15 +11,15 @@ class CupertinoExample extends StatelessWidget {
         title: const Text('Заголовок'),
         message: const Text('Сообщение'),
         actions: [
-          TextButton(
+          CupertinoButton(
             onPressed: () => debugPrint('OK'),
             child: const Text('OK'),
           ),
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('Закрыть'),
-          ),
         ],
+        cancelButton: CupertinoButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Отмена'),
+        ),
       ),
     );
   }
@@ -31,15 +31,44 @@ class CupertinoExample extends StatelessWidget {
         title: const Text('Заголовок'),
         content: const Text('Содержание'),
         actions: [
-          TextButton(
+          CupertinoButton(
             onPressed: () => debugPrint('OK'),
             child: const Text('OK'),
           ),
-          TextButton(
+          CupertinoButton(
             onPressed: () => Navigator.pop(context),
             child: const Text('Закрыть'),
           ),
         ],
+      ),
+    );
+  }
+
+  void _showPicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => CupertinoPicker(
+        itemExtent: 32.0,
+        onSelectedItemChanged: (value) => debugPrint(
+          value.toString(),
+        ),
+        children: List<Widget>.generate(
+          10,
+          (index) => Text('Вариант $index'),
+        ),
+      ),
+    );
+  }
+
+  void _printDuration(Duration duration) {
+    debugPrint('${duration.inSeconds} секунд');
+  }
+
+  void _showTimePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => CupertinoTimerPicker(
+        onTimerDurationChanged: _printDuration,
       ),
     );
   }
@@ -61,6 +90,62 @@ class CupertinoExample extends StatelessWidget {
               CupertinoButton(
                 onPressed: () => _showModal(context),
                 child: const Text('Показать диалог'),
+              ),
+              CupertinoButton(
+                onPressed: () => _showPicker(context),
+                child: const Text('Показать picker'),
+              ),
+              CupertinoButton(
+                onPressed: () => _showTimePicker(context),
+                child: const Text('Показать TimePicker'),
+              ),
+              Center(
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: CupertinoContextMenu(
+                    actions: <Widget>[
+                      CupertinoContextMenuAction(
+                        onPressed: () {
+                          debugPrint('Скопировано');
+                          Navigator.pop(context);
+                        },
+                        isDefaultAction: true,
+                        trailingIcon: CupertinoIcons.doc_on_clipboard_fill,
+                        child: const Text('Copy'),
+                      ),
+                      CupertinoContextMenuAction(
+                        onPressed: () {
+                          debugPrint('Расшарено');
+                          Navigator.pop(context);
+                        },
+                        trailingIcon: CupertinoIcons.share,
+                        child: const Text('Share'),
+                      ),
+                      CupertinoContextMenuAction(
+                        onPressed: () {
+                          debugPrint('Добавлено в избранные');
+                          Navigator.pop(context);
+                        },
+                        trailingIcon: CupertinoIcons.heart,
+                        child: const Text('Favorite'),
+                      ),
+                      CupertinoContextMenuAction(
+                        onPressed: () {
+                          debugPrint('Удалено');
+                          Navigator.pop(context);
+                        },
+                        isDestructiveAction: true,
+                        trailingIcon: CupertinoIcons.delete,
+                        child: const Text('Delete'),
+                      ),
+                    ],
+                    child: Container(
+                      color: CupertinoColors.systemYellow,
+                      child: const FlutterLogo(size: 500.0),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
